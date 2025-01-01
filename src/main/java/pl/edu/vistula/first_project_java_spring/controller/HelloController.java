48dbf56e017735ff3912d0b2b793fec6186f8354
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2025-01-01 19:29:29 +0100

    Add HelloController

---
+++ src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Vistula";
+    }
+}
