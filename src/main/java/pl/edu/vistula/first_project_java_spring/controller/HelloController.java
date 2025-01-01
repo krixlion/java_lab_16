65d420c85a76d4f51a833b4bab861d4ed1a412c6
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2025-01-01 19:31:13 +0100

    Add greeting handler

--- src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
+++ src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
@@ -1,13 +1,22 @@
 package pl.edu.vistula.first_project_java_spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello Vistula";
+        return "hello";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
